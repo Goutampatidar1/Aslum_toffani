1f666787822ccd07d3295f00c3a8130cc641c95a
Goutampatidar1 <op.goutampatidar001@gmail.com>
2025-09-22 13:05:32 +0530

    camera mmodel updated

--- app/models/camera_model.py
+++ app/models/camera_model.py
@@ -1,8 +1,18 @@
 class Camera:
-    def __init__(self, camera_name, camera_place, url, comapny_id=None):
+    def __init__(
+        self,
+        camera_name,
+        camera_place,
+        url,
+        company_name,
+        company_email,
+        comapny_id,
+    ):
         self.company_id = comapny_id
         self.camera_name = camera_name
         self.camera_place = camera_place
+        self.company_name = company_name
+        self.company_email = company_email
         self.url = url
 
     def to_dict(self):
@@ -10,5 +20,7 @@ class Camera:
             "company_id": self.company_id,
             "camera_name": self.camera_name,
             "camera_place": self.camera_place,
+            "company_name": self.company_name,
+            "company_email": self.company_email,
             "url": self.url,
         }
